cb44c465e6d6c2c1483f043062aa87e0a7fb79ad
azisdzaelani <azisdzelani@mhs.uinjkt.ac.id>
2018-05-19 21:10:07 +0700

    asp.net crud

---
+++ MVCCRUD/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MVCCRUD.Models;
+
+namespace MVCCRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        string ConnectionString = @"Data Source=.; Initial Catalog=MVCCRUDDB;Integrated Security=true";
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DataTable dtblProduct = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Product ORDER BY ProductID desc", sqlCon);
+                sqlDa.Fill(dtblProduct);
+            }
+            return View(dtblProduct);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View(new ProductModel());
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel productModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                string query = "INSERT INTO Product VALUES(@ProductName, @Price, @Count)";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+                sqlCmd.Parameters.AddWithValue("@Price", productModel.Price);
+                sqlCmd.Parameters.AddWithValue("@Count", productModel.Count);
+                sqlCmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProductModel productModel = new ProductModel();
+            DataTable dtblProduct = new DataTable();
+
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                string query = "SELECT * FROM Product WHERE ProductID = @ProductID";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@ProductID", id);
+                sqlDa.Fill(dtblProduct);
+                
+                if(dtblProduct.Rows.Count == 1)
+                {
+                    productModel.ProductID = Convert.ToInt32(dtblProduct.Rows[0][0].ToString());
+                    productModel.ProductName = dtblProduct.Rows[0][1].ToString();
+                    productModel.Price = Convert.ToDecimal(dtblProduct.Rows[0][2]);
+                    productModel.Count = Convert.ToInt32(dtblProduct.Rows[0][3]);
+                    return View(productModel);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ProductModel productModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                string query = "UPDATE Product SET ProductName = @ProductName, Price = @Price, Count = @Count WHERE ProductID = @ProductID";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@ProductID", productModel.ProductID);
+                sqlCmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+                sqlCmd.Parameters.AddWithValue("@Price", productModel.Price);
+                sqlCmd.Parameters.AddWithValue("@Count", productModel.Count);
+                sqlCmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(ConnectionString))
+            {
+                sqlCon.Open();
+                string query = "DELETE FROM Product WHERE ProductID = @ProductID";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@ProductID", id);
+                sqlCmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ MVCCRUD/Models/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace MVCCRUD.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+        [DisplayName("Quantity")]
+        public int Count { get; set; }
+    }
+}
